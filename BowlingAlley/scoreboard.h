7d5ca426d236bb8bfaad37eaad7bba674fb73ae3
spartanny <tq_29@hotmail.com>
2022-01-30 02:49:31 +0530

    display winner in output

--- BowlingAlley/scoreboard.h
+++ BowlingAlley/scoreboard.h
@@ -15,12 +15,16 @@ public:
         score[player] = score[player] + chance + ", ";
     }
     void showScore(vector<Player> players)
-    {
+    {   
+        pair<int,int> result = {0,0}; // pair to store winner player and their score
         for (auto iter : score)
         {
             int id = iter.first;
             cout << id << " : " << iter.second << " " << players[id].getScore();
             cout << "\n";
+            if(players[id].getScore() > result.second)
+                result.first = id, result.second = players[id].getScore();
         }
+        cout<<"Winner is Player-"<<result.first<<" with score "<<result.second<<"\n";
     }
 };
